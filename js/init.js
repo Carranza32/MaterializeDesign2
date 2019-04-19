8432cdfdaec988687d67b376ca6c2f4463a0a863
Mario <mario.carranza996@gmail.com>
2019-04-19 12:56:37 -0600

    Official 1.0 version

--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    M.AutoInit();
     $('.chips-placeholder').chips({
         placeholder: 'Enter a tag',
         secondaryPlaceholder: '+Tag',
@@ -7,6 +8,7 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $('.select-wrapper').siblings("label").addClass("active");
     $('.modal').modal({inDuration: 150,outDuration: 150});
+    $('#tabs-swipe-demo').tabs({swipeable: true});
     //$(".rating").rate();
     $('input.autocomplete').autocomplete({
         data: {
@@ -16,17 +18,24 @@ $(document).ready(function () {
         },
     });
 
-    const menu = ["Button","Card","Checkbox","Chips","Dialog","Drawer","Elevation","Fab","Image List","List","Progress","Menu","Radio","Ripple","Select","Slider","Snackbar","Switch","Tab Bar","Text Field","Nav","Typography"];
-    for (let i = 0; i < menu.length; i++) {
-        $("#slide-out").append("<li><a id='"+menu[i]+"' href='"+menu[i]+".html' class='waves-effect'>"+menu[i]+"</a></li>");
-    }
+    $('input#input_text, textarea#textarea2').characterCounter();
 
-    $("ul#slide-out > li > a").click(function(e){
-        /*e.preventDefault();*/
-    });
+    $(".dropdown-trigger").dropdown({coverTrigger: false});
+    
+
+    //Menu completo const menu = ["Getting Started","Button","Card","Checkbox","Chips","Dialog","Drawer","Elevation","Fab","Image List","List","Progress","Menu","Radio","Ripple","Select","Slider","Snackbar","Switch","Tab Bar","Text Field","Nav","Typography"];
+    const menu = ["Getting Started","Button","Card","Selection","Chips","Dialog","Drawer","Elevation","Select","Snackbar","Tab Bar","Text Field","Nav","Typography"];
+    for (let i = 0; i < menu.length; i++) {
+        $("#slide-out").append("<li><a id='"+menu[i]+"' href='"+menu[i].replace(" ","")+".html' class='waves-effect'>"+menu[i]+"</a></li>");
+    }
 
     $("#limpiar").click(function (e) { 
         e.preventDefault();
         $("#search").val('');
     });
 });
+
+function MakeToast(){
+    var toastHTML = '<span>Single-line message with action.</span><button class="btn-flat toast-action">Action</button>';
+    M.toast({html: toastHTML});
+}
